36ce31d13ddcb6ba65b4ed38eaa4730df1a22df7
Thomas Perl <m@thp.io>
2014-03-29 22:33:09 +0100

    gPodder 4.1.0 "Moombahcore" released

--- src/gpodder/__init__.py
+++ src/gpodder/__init__.py
@@ -50,9 +50,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # This metadata block gets parsed by setup.py - use single quotes only
 __tagline__ = 'Media and podcast aggregator'
 __author__ = 'Thomas Perl <thp@gpodder.org>'
-__version__ = '4.0.0'
-__date__ = '2014-02-14'
-__relname__ = 'Iffy Kiffy Izzy Oh'
+__version__ = '4.1.0'
+__date__ = '2014-03-29'
+__relname__ = 'Moombahcore'
 __copyright__ = '© 2005-2014 Thomas Perl and the gPodder Team'
 __license__ = 'ISC / GPLv3 or later'
 __url__ = 'http://gpodder.org/'
